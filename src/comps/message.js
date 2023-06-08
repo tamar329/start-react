6f287274e739db4bb4b3b57be226d3b578a7937b
tamar329 <tamar7475@gmail.com>
2023-06-08 11:33:26 +0300

    add appHw and message

---
+++ src/comps/message.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Massage extends Component {
+    render() {
+        return (
+            <div className="p-2" style={{background:this.props.bg, border:"2px solid blue"}}>
+                <h2>Message: {this.props.msg}</h2>
+            </div>
+        )
+    }
+}
